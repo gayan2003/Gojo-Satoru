34d309a343ed54d9d9124180f403c461804a6757
gayan2003 <94740662+gayan2003@users.noreply.github.com>
2022-06-07 19:58:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94774160259']
+global.premium = ['94774160259']
+global.ownernomer = '94774160259'
+global.ownername = 'Lucifer md'
+global.botname = 'Lucifer md'
+global.footer = 'Lucifer md'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'sri lanka, north central, dambulla'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🥰','😁','🤭']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
